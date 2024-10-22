3f8306aba49dc0a722055ce6ea7ff6d5f2695c90
Esteban S <esteban.sanchez.nt@gmail.com>
2024-10-22 13:45:38 -0500

    Refactor Dockerfile and entrypoint.sh for BE service

---
+++ be/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Run database migrations
+echo "Running migrations..."
+./node_modules/.bin/knex migrate:latest --env production
+
+if [ $? -ne 0 ]; then
+    echo "Migrations failed. Exiting..."
+    exit 1
+fi
+
+# Run database seeds
+echo "Running seeds..."
+./node_modules/.bin/knex seed:run --env production
+
+if [ $? -ne 0 ]; then
+    echo "Seeding failed. Exiting..."
+    exit 1
+fi
+
+# Start the application
+echo "Starting the application..."
+node dist/index.js
